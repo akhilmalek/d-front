4a102dc5786facbe46befe2d8d49bb2f6511ee1d
Akhil.Malek <akhil.malek@archetypegrowth.com>
2024-02-27 16:06:26 +0530

    initial file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,14 @@ import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
 import Main from './common-layout/main'
 
+interface PageProps {
+  // Define the type of pageProps
+  // Example: You can define it as an object with certain properties
+  // Adjust according to your actual pageProps type
+  prop1: string;
+  prop2: number;
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Main>
